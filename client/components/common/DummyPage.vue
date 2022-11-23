b0121332ca8e89ea618b559c72b4e47f85ed3f19
Peihua Huang <peihua@mit.edu>
2022-11-23 15:43:07 -0500

    updated dummy page

--- client/components/common/DummyPage.vue
+++ client/components/common/DummyPage.vue
@@ -1,34 +1,34 @@
+<!-- Dummy default page -->
+
+<template>
+  <main>
+    <section v-if="$store.state.username">
+      <header>
+        <h2>Welcome @{{ $store.state.username }}</h2>
+      </header>
+      <DummyForm />
+    </section>
+    <section v-else>
+      <header>
+        <h2>Welcome to GoodFood!</h2>
+      </header>
+      <article>
+        <h3>
+          <router-link to="/login">
+            Sign in
+          </router-link>
+          to manage your groceries.
+        </h3>
+      </article>
+    </section>
+  </main>
+</template>
+
 <script>
-import BlockForm from '@/components/common/BlockForm.vue';
+import DummyForm from '@/components/common/DummyForm.vue';
 
 export default {
   name: 'DummyPage',
-  mixins: [BlockForm],
-  data() {
-    return {
-      url: '/dummy',
-      method: 'POST',
-      hasBody: true,
-      fields: [
-        { type: 'text', id: 'username', label: 'Username', value: '' }, 
-        { type: 'password', id: 'password', label: 'Password', value: '' },
-        { type: 'content', id: 'description', label: 'Description', value: '', placeholder: 'Start typing...' },
-        { type: 'collection', id: 'tags', label: 'Tags', value: '', placeholder: 'Type tag and press enter...', collection: [] },
-        { type: 'quantity', id: 'quantity', label: 'Quantity', value: '', placeholder: 'Enter number...', unit: '' },
-        { type: 'numerical', id: 'remindMe', label: 'Remind Me', value: '', placeholder: 'Enter number...' },
-        { type: 'date', id: 'expirationDate', label: 'Expiration Date', value: '', required: false },
-        { type: 'ingredients', id: 'ingredients', label: 'Ingredients', name: '', quantity: '', unit: '', ingredients: [] },
-        { type: 'baskets', id: 'baskets', label: 'Baskets' }
-      ],
-      title: 'Example Form',
-      callback: () => {
-        const message = 'Successfully submitted form!';
-        this.$store.commit('alert', {
-          message: message,
-          status: 'success'
-        });
-      }
-    };
-  }
+  components: {DummyForm},
 };
 </script>

415c11d26e2adf5c560a209600c6a5c08fefbb3e
Peihua Huang <peihua@mit.edu>
2022-12-13 16:34:25 -0500

    added button for filtering

--- client/components/PantryItem/PantryItemsPage.vue
+++ client/components/PantryItem/PantryItemsPage.vue
@@ -4,18 +4,27 @@
   <main>
     <AddPantryItemForm />
     <hr>
-    <h1>My Pantry <i class="bi bi-info-circle" data-toggle="tooltip" data-placement="right" title="Pantry items are displayed by ascending expiration date. If no expiration dates are provided, the items will be displayed by ascending date added after the items with expiration dates."></i></h1> 
-    <p>Keep track and manage the items in your pantry here.</p>
-    <section
-      v-if="$store.state.pantryItems.filter(pantryItem => pantryItem.inPantry === 'true').length"
-    >
+    <h1>My Pantry <i class="bi bi-info-circle" data-toggle="tooltip" data-placement="right"
+        title="Pantry items are displayed by ascending expiration date. If no expiration dates are provided, the items will be displayed by ascending date added after the items with expiration dates."></i>
+    </h1>
+    <section>
+      <p>Keep track and manage the items in your pantry here.</p>
+      <header class="inline">
+        <h3>
+          <i>Viewing {{ expiringItemsOnly ? 'expiring items only' : 'all pantry items' }}</i>
+          <button v-if="expiringItemsOnly" class="btn btn-primary mr-2 my-2 right" @click="toggleDisplay">
+            View all pantry items
+          </button>
+          <button v-else class="btn btn-primary mr-2 my-2 right" @click="toggleDisplay">
+            View expiring items only
+          </button>
+        </h3>
+      </header>
+    </section>
+    <section v-if="$store.state.pantryItems.filter(pantryItem => pantryItem.inPantry === 'true').length">
       <PantryItemComponent
-        v-for="item in $store.state.pantryItems.filter(pantryItem => pantryItem.inPantry === 'true').sort((item1, item2) => (item1.expirationDate ? item1.expirationDate : 'a' + item1.dateAdded).localeCompare(item2.expirationDate ? item2.expirationDate : 'a' + item2.dateAdded))"
-        :key="item._id"
-        :pantryItem="item"
-        :isPantry="true"
-        :reminder="$store.state.reminders[item._id]"
-      />
+        v-for="item in $store.state.pantryItems.filter(pantryItem => expiringItemsOnly ? pantryItem.inPantry === 'true' && pantryItem.expirationDate : pantryItem.inPantry === 'true').sort((item1, item2) => (item1.expirationDate ? item1.expirationDate : 'a' + item1.dateAdded).localeCompare(item2.expirationDate ? item2.expirationDate : 'a' + item2.dateAdded))"
+        :key="item._id" :pantryItem="item" :isPantry="true" :reminder="$store.state.reminders[item._id]" />
     </section>
   </main>
 </template>
@@ -27,9 +36,19 @@ import AddPantryItemForm from '@/components/PantryItem/AddPantryItemForm.vue';
 export default {
   name: 'PantryItemsPage',
   components: { PantryItemComponent, AddPantryItemForm },
+  data() {
+    return {
+      expiringItemsOnly: false,
+    }
+  },
   mounted() {
     this.$store.commit('refreshPantryItems');
     this.$store.commit('refreshReminders');
+  },
+  methods: {
+    toggleDisplay() {
+      this.expiringItemsOnly = !this.expiringItemsOnly;
+    }
   }
 };
 </script>
@@ -54,5 +73,13 @@ section .scrollbox {
   flex: 1 0 50vh;
   padding: 3%;
   overflow-y: scroll;
+}
+
+.right {
+  float: right;
+}
+
+.inline {
+    display: inline;
 }
 </style>
